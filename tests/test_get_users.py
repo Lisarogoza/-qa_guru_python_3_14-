2f195fcfa5dc1a8420047135abadc155aaa1fb30
Ксения <118900137+Lisarogoza@users.noreply.github.com>
2023-03-22 15:37:01 +0300

    Added test get users file

---
+++ tests/test_get_users.py
@@ -0,0 +1,30 @@
+import requests
+from pytest_voluptuous import S
+from schemas.user import users_schema
+import logging
+
+
+def test_get_users_list():
+    result = requests.get('https://reqres.in/api/users?page=2')
+    logging.info(result.json())
+    assert S(users_schema) == result.json()
+
+
+def test_count_page():
+    result = requests.get('https://reqres.in/api/users?page=2')
+    per_page = result.json()['per_page']
+    data = result.json()['data']
+
+    assert per_page == 6
+    assert len(data) == 6
+    assert S(users_schema) == result.json()
+
+
+def test_post_user():
+    url = 'https://reqres.in/api/users'
+    data = {'name': 'Xman', 'age': '97'}
+    response = requests.post(url, data=data)
+
+    assert response.status_code == 201
+    assert response.json()['name'] == 'Xman'
+    assert response.json()['age'] == '97'
